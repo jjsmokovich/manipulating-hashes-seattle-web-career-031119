ba664b10b9be0062ca91b95e826aa466518553c8
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-06 10:40:17 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,17 +1,4 @@
-def first_challenge
- "Jon Snow" => {
-    name: "Jon",
-    email: "jon_snow@thewall.we", 
-    favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
-    knows: nil
-  },
-  "Freddy Mercury" => {
-    name: "Freddy",
-    email: "freddy@mercury.com",
-    favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  }
-}
-  
+
   
 
   #remember to return your newly altered contacts hash!
